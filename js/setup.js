8f0327fcad149befb1afafb45fce034216f66046
Anton Lapshin <anton.lapshin.s@gmail.com>
2017-12-01 10:28:45 +0200

    Rearranged the code

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,5 @@
 'use strict';
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var totalNumberOfWizards = 4;
 var similarWizards = [];
 
 var getRandomArrayItem = function (arr) {
@@ -12,6 +7,10 @@ var getRandomArrayItem = function (arr) {
 };
 
 var generateCharacter = function () {
+  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   return {
     name: getRandomArrayItem(NAMES) + ' ' + getRandomArrayItem(SURNAMES),
     coatColor: getRandomArrayItem(COATCOLORS),
@@ -19,13 +18,7 @@ var generateCharacter = function () {
   };
 };
 
-var setup = document.querySelector('.setup');
-setup.classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-var renderWizard = function (wizard) {
+var renderWizardElements = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
@@ -33,11 +26,19 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < totalNumberOfWizards; i++) {
-  similarWizards[i] = generateCharacter();
-  fragment.appendChild(renderWizard(similarWizards[i]));
-}
-similarListElement.appendChild(fragment);
+var renderWizards = function (totalNumberOfWizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < totalNumberOfWizards; i++) {
+    similarWizards[i] = generateCharacter();
+    fragment.appendChild(renderWizardElements(similarWizards[i]));
+  }
+  return fragment;
+};
 
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+similarListElement.appendChild(renderWizards(4));
 document.querySelector('.setup-similar').classList.remove('hidden');
