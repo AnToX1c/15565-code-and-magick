aa7d47424b63c11024c4b98975d4406f56efc229
Anton Lapshin <anton.lapshin.s@gmail.com>
2017-12-19 13:29:49 +0200

    Updated similar wizards list, debounce / Completed the task #7

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,9 +1,13 @@
 'use strict';
 
 (function () {
+  var getRandomArrayItem = function (arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
   window.colorizeElement = function (elem, colors, callback) {
     elem.addEventListener('click', function () {
-      callback(elem, window.getRandomArrayItem(colors));
+      callback(elem, getRandomArrayItem(colors));
     });
   };
 })();
