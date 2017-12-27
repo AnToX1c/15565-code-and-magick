95c094b0c7052f1e8f8102a37a985cf5b1a189e7
Anton Lapshin <anton.lapshin.s@gmail.com>
2017-12-27 16:26:39 +0200

    Deleted file types verification

--- js/avatar.js
+++ js/avatar.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var FILE_TYPES = ['image/jpeg', 'image/gif', 'image/png'];
+  // var FILE_TYPES = ['image/jpeg', 'image/gif', 'image/png'];
   var fileChooser = document.querySelector('.upload input[type=file]');
   var preview = document.querySelector('.setup-user-pic');
 
@@ -10,18 +10,18 @@
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
 
-    var matches = FILE_TYPES.some(function (it) {
-      return file.type === it;
+    // var matches = FILE_TYPES.some(function (it) {
+    //   return file.type === it;
+    // });
+
+    // if (matches) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      preview.src = reader.result;
     });
 
-    if (matches) {
-      var reader = new FileReader();
-
-      reader.addEventListener('load', function () {
-        preview.src = reader.result;
-      });
-
-      reader.readAsDataURL(file);
-    }
+    reader.readAsDataURL(file);
+    // }
   });
 })();
