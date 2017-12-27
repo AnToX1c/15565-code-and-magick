6bd6a67ab3de10c5806fcd89c7fe3456c5155ce1
Anton Lapshin <anton.lapshin.s@gmail.com>
2017-12-27 13:15:53 +0200

    Added 'accept' attribute for input

--- js/avatar.js
+++ js/avatar.js
@@ -1,16 +1,17 @@
 'use strict';
 
 (function () {
-  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var FILE_TYPES = ['image/jpeg', 'image/gif', 'image/png'];
   var fileChooser = document.querySelector('.upload input[type=file]');
   var preview = document.querySelector('.setup-user-pic');
 
+  fileChooser.setAttribute('accept', 'image/*');
+
   fileChooser.addEventListener('change', function () {
     var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
+      return file.type === it;
     });
 
     if (matches) {
